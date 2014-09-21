23b124b07d9148477627553302093caba424898c
mikeloeven <mikeloeven@gmail.com>
2014-09-21 02:56:38 -0400

    .

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140920055845) do
+ActiveRecord::Schema.define(version: 20140921020947) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
