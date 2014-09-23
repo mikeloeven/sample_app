293a430930e04388d4534aeebed91fb88ba51d24
mikeloeven <mikeloeven@gmail.com>
2014-09-23 14:34:41 -0400

    final submission

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,10 @@ class User < ActiveRecord::Base
     Micropost.where("user_id = ?", id)
   end
 
+  def feed
+    Micropost.from_users_followed_by(self)
+  end
+
   def following?(other_user)
     relationships.find_by(followed_id: other_user.id)
   end
